c02a22ee208ed55854096ef0e6e89dc8dd24b749
Guille Lopez <guillelopezgines@gmail.com>
2018-04-26 10:20:08 +0200

    Removed all old files

--- config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-Guillelopez::Application.config.session_store :cookie_store, key: '_guillelopez_session'
--- config/routes.rb
+++
@@ -1,5 +0,0 @@
-Guillelopez::Application.routes.draw do
-  get 'feed', to: 'home#feed'
-  get 'rss.php', to: 'home#feed', format: 'rss'
-  root 'home#index'
-end
